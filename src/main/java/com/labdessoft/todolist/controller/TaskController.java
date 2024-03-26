37325a4dd43aa959655866c42cff5d2b7c6846ef
duda-amaral <dudaamaralmap3110@gmail.com>
2024-03-25 22:39:53 -0300

    feat: add crud to task controller

--- src/main/java/com/labdessoft/todolist/controller/TaskController.java
+++ src/main/java/com/labdessoft/todolist/controller/TaskController.java
@@ -1,16 +1,69 @@
 package com.labdessoft.todolist.controller;
 
+import com.labdessoft.todolist.entity.Task;
+import com.labdessoft.todolist.service.TaskService;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
+@RequestMapping("/task")
 public class TaskController {
 
-    @GetMapping("/task")
-    @Operation(summary = "Lista todas as tarefas")
-    public String listAll() {
-    return "listar todas as tasks";
+    @Autowired
+    private TaskService taskService;
+    @GetMapping
+    @Operation(description = "Lista todas as tarefas cadastradas")
+    public ResponseEntity<List<Task>> listAll() {
+        try{
+            List<Task> taskList = this.taskService.listAll();
+
+            if(taskList.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(taskList, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    @PostMapping
+    @Operation(description = "Cadastra as tarefas")
+    public ResponseEntity<Task> create(@Valid @RequestBody Task obj) {
+        this.taskService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @Operation(description = "Atualiza uma tarefa")
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> update(@Valid @RequestBody Task obj, @PathVariable Long id) {
+        obj.setId(id);
+        this.taskService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(description = "Atualiza o status de uma tarefa")
+    @PutMapping("/{id}/completed")
+    public ResponseEntity<Task> updateStatus(@Valid @RequestBody Task obj, @PathVariable Long id) {
+        obj.setId(id);
+        obj.setCompleted(true);
+        this.taskService.updateStatus(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(description = "Deleta uma tarefa")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.taskService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
