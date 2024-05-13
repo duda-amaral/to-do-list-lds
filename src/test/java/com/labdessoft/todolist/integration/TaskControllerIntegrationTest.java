92e47c463da9419649ccb59175397a8e0c67799a
Maria Eduarda <dudaamaralmap3110@gmail.com>
2024-05-13 07:36:41 -0300

    feat: add integration tests

---
+++ src/test/java/com/labdessoft/todolist/integration/TaskControllerIntegrationTest.java
@@ -0,0 +1,41 @@
+package com.labdessoft.todolist.integration;
+
+import com.labdessoft.todolist.ToDoListApplication;
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.equalTo;
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
+@SpringBootTest(classes = {ToDoListApplication.class}, webEnvironment
+        = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
+public class TaskControllerIntegrationTest {
+    @Before
+    public void setup() {
+        RestAssured.baseURI = "https://to-do-list-lds.onrender.com";
+        RestAssured.port = 8080;
+    }
+
+    @Test
+    public void
+    givenUrl_whenSuccessOnGetsResponseAndJsonHasRequiredKV_thenCorrect() {
+        get("/api/task").then().statusCode(200);
+    }
+
+    @Test
+    public void
+    givenUrl_whenSuccessOnGetsResponseAndJsonHasOneTask_thenCorrect() {
+        get("/api/task/6").then().statusCode(200)
+                .assertThat().body("description",
+                        equalTo("Primeira tarefa"));
+    }
+}
