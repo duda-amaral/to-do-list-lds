a8a65a7506a48b0db573ec7dd87f02346cbf404f
duda-amaral <dudaamaralmap3110@gmail.com>
2024-05-28 22:35:18 -0300

    Update ToDoListApplicationTests.java

--- src/test/java/com/labdessoft/todolist/ToDoListApplicationTests.java
+++ src/test/java/com/labdessoft/todolist/ToDoListApplicationTests.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ToDoListApplicationTests {
 
-    @Test
+   /* @Test
     void contextLoads() {
-    }
-
+    }*/
 }
