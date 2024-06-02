c4ce922e6739f0efa870904e579d4f2b66beac9c
duda-amaral <dudaamaralmap3110@gmail.com>
2024-06-01 21:13:57 -0300

    feat(TaskDataServiceTest): adicionando testes para TaskData

--- src/main/java/com/labdessoft/todolist/entity/TaskData.java
+++ src/main/java/com/labdessoft/todolist/entity/TaskData.java
@@ -1,24 +1,38 @@
 package com.labdessoft.todolist.entity;
 
+import com.labdessoft.todolist.enums.Prioridade;
+import com.labdessoft.todolist.enums.TaskTipo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Schema(description = "Todos os detalhes de uma tarefa do tipo data")
 @Table(name = "TASK_DATA")
 public class TaskData extends Task{
 
     @Schema(description = "Data prevista de conclusão")
     private LocalDate dueDate;
+
+    @Builder(builderMethodName = "taskDataBuilder")
+    public TaskData(Long id, @Size(min = 10, message = "Descrição da tarefa deve possuir pelo menos 10 caracteres") String description,
+                    Boolean completed,
+                    Prioridade priority,
+                    TaskTipo type,
+                    String status,
+                    LocalDate dueDate) {
+        super(id, description, completed, priority, type, status);
+        this.dueDate = dueDate;
+    }
+
+    public TaskData() {
+
+    }
 }
--- src/test/java/com/labdessoft/todolist/mock/TaskMock.java
+++ src/test/java/com/labdessoft/todolist/mock/TaskMock.java
@@ -1,14 +1,14 @@
 package com.labdessoft.todolist.mock;
 
 import com.labdessoft.todolist.entity.Task;
+import com.labdessoft.todolist.entity.TaskData;
 import lombok.experimental.UtilityClass;
 
-import java.util.UUID;
+import java.time.LocalDate;
 
-import static com.labdessoft.todolist.enums.Prioridade.ALTA;
-import static com.labdessoft.todolist.enums.Prioridade.BAIXA;
+import static com.labdessoft.todolist.enums.Prioridade.*;
+import static com.labdessoft.todolist.enums.TaskTipo.DATA;
 import static com.labdessoft.todolist.enums.TaskTipo.LIVRE;
-import static java.util.UUID.randomUUID;
 
 @UtilityClass
 public class TaskMock {
@@ -29,6 +29,28 @@ public class TaskMock {
                 .priority(ALTA)
                 .completed(false)
                 .type(LIVRE)
+                .build();
+    }
+
+    public static TaskData createTaskData() {
+        return TaskData.taskDataBuilder()
+                .id(50L)
+                .description("tarefa de ihc")
+                .priority(MÉDIA)
+                .completed(false)
+                .type(DATA)
+                .dueDate(LocalDate.of(2024, 06, 04))
+                .build();
+    }
+
+    public static TaskData updateTaskData() {
+        return TaskData.taskDataBuilder()
+                .id(50L)
+                .description("tarefa de ihc atualizada")
+                .priority(ALTA)
+                .completed(false)
+                .type(DATA)
+                .dueDate(LocalDate.of(2024, 06, 06))
                 .build();
     }
 }
