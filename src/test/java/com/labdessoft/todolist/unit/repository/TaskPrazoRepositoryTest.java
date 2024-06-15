e8b335d5bbb0bd96d0dcaf2e150bdc722e2b4eb2
duda-amaral <dudaamaralmap3110@gmail.com>
2024-06-15 16:01:14 -0300

    adicionando teste para taskprazo repository

--- src/test/java/com/labdessoft/todolist/unit/repository/TaskPrazoRepositoryTest.java
+++ src/test/java/com/labdessoft/todolist/unit/repository/TaskPrazoRepositoryTest.java
@@ -1,9 +1,9 @@
 package com.labdessoft.todolist.unit.repository;
 
-import com.labdessoft.todolist.entity.TaskData;
+import com.labdessoft.todolist.entity.TaskPrazo;
 import com.labdessoft.todolist.enums.Prioridade;
 import com.labdessoft.todolist.enums.TaskTipo;
-import com.labdessoft.todolist.repository.TaskDataRepository;
+import com.labdessoft.todolist.repository.TaskPrazoRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,7 +15,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,58 +24,58 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(JUnitPlatform.class)
 @ActiveProfiles("test")
 @DataJpaTest
-public class TaskDataRepositoryTest {
+public class TaskPrazoRepositoryTest {
     @Autowired
-    private TaskDataRepository taskRepository;
+    private TaskPrazoRepository taskRepository;
 
     @Autowired
     private TestEntityManager entityManager;
 
-    private TaskData task;
+    private TaskPrazo task;
 
     @BeforeEach
     public void setup() {
 
-        task = TaskData.taskDataBuilder()
+        task = TaskPrazo.taskPrazoBuilder()
                 .description("This is a test task")
                 .completed(false)
                 .priority(Prioridade.ALTA)
-                .type(TaskTipo.DATA)
+                .type(TaskTipo.PRAZO)
                 .status("Pendente")
-                .dueDate(LocalDate.of(2025, 06, 04))
+                .dueDays(10)
                 .build();
 
         entityManager.persist(task);
     }
 
     @Test
-    public void whenFindById_thenReturnTaskData() {
-        Optional<TaskData> foundTask = taskRepository.findById(task.getId());
+    public void whenFindById_thenReturnTaskPrazo() {
+        Optional<TaskPrazo> foundTask = taskRepository.findById(task.getId());
 
         assertThat(foundTask).isPresent();
         assertThat(foundTask.get().getDescription()).isEqualTo(task.getDescription());
     }
 
     @Test
-    public void whenFindAll_thenReturnTaskDataList() {
-        List<TaskData> tasks = taskRepository.findAll();
+    public void whenFindAll_thenReturnTaskPrazoList() {
+        List<TaskPrazo> tasks = taskRepository.findAll();
 
         assertThat(tasks).hasSize(1);
         assertThat(tasks.get(0).getDescription()).isEqualTo(task.getDescription());
     }
 
     @Test
-    public void whenSave_thenReturnSavedTaskData() {
-        TaskData newTask = TaskData.taskDataBuilder()
+    public void whenSave_thenReturnSavedTaskPrazo() {
+        TaskPrazo newTask = TaskPrazo.taskPrazoBuilder()
                 .description("New Task Description")
                 .completed(false)
                 .priority(Prioridade.ALTA)
                 .type(TaskTipo.LIVRE)
                 .status("Pendente")
-                .dueDate(LocalDate.of(2025, 06, 04))
+                .dueDays(10)
                 .build();
 
-        TaskData savedTask = taskRepository.save(newTask);
+        TaskPrazo savedTask = taskRepository.save(newTask);
 
         assertThat(savedTask).isNotNull();
         assertThat(savedTask.getId()).isNotNull();
@@ -84,18 +83,19 @@ public class TaskDataRepositoryTest {
     }
 
     @Test
-    public void whenDelete_thenRemoveTaskData() {
+    public void whenDelete_thenRemoveTaskPrazo() {
         taskRepository.delete(task);
 
-        Optional<TaskData> foundTask = taskRepository.findById(task.getId());
+        Optional<TaskPrazo> foundTask = taskRepository.findById(task.getId());
         assertThat(foundTask).isNotPresent();
     }
 
     @Test
-    public void whenUpdate_thenReturnUpdatedTaskData() {
+    public void whenUpdate_thenReturnUpdatedTaskPrazo() {
         task.setDescription("Updated Task Description");
-        TaskData updatedTask = taskRepository.save(task);
+        TaskPrazo updatedTask = taskRepository.save(task);
 
         assertThat(updatedTask.getDescription()).isEqualTo("Updated Task Description");
     }
+
 }
