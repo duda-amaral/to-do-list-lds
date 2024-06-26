726fc7e40314a2451efaa23093c1db4be2bd1ecb
duda-amaral <dudaamaralmap3110@gmail.com>
2024-06-25 22:12:31 -0300

    corrigindo erro de cors 4

--- src/main/java/com/labdessoft/todolist/security/WebConfig.java
+++ src/main/java/com/labdessoft/todolist/security/WebConfig.java
@@ -27,7 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("https://to-do-list-front-end-c4oy.onrender.com");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
